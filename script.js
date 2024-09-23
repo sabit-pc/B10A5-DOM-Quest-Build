81eb857a1ac4edb7c8b10132ef17b54a3a389b11
sabit-pc <sabitmiah29@gmail.com>
2024-09-23 20:34:00 +0600

    common fuction added to all cards

--- script.js
+++ script.js
@@ -4,25 +4,29 @@ document.getElementById("history-btn").addEventListener('click', function(){
     document.getElementById("history-btn").classList.add('focus:bg-bg-primary-0');
     document.getElementById("donation-btn").classList.remove('bg-bg-primary-0');
     document.getElementById("donation-btn").classList.add('bg-stone-100');
+    document.getElementById("hide-history-from-home").classList.remove('hidden');
 })
 
 document.getElementById("donation-btn").addEventListener('click', function(){
     document.getElementById("margin-set").classList.remove('hidden');
     document.getElementById("donation-btn").classList.add('bg-bg-primary-0');
     document.getElementById("history-btn").classList.remove('bg-bg-primary-0');
+    document.getElementById("hide-history-from-home").classList.add('hidden');
 })
 
 
-function donationCalculator () {
-    let myAmount = parseFloat(document.getElementById("my-amount").value);
-    let currentBalance = parseFloat(document.getElementById("current-balance").innerText);
-    let donatedMoney = parseFloat(document.getElementById("donated-money").innerText);
-    const historyContainer = document.getElementById("history-container");
-    const uniqueText = document.getElementById('unique-text').innerText;
 
-    if (myAmount <= currentBalance && !isNaN(myAmount)) {
+function donationCalculator (itemIndex) {
+    const historyContainer = document.getElementById("history-container");
+    const uniqueText = document.getElementById(`unique-text-${itemIndex}`).innerText;
+    let myAmount = parseFloat(document.getElementById(`my-amount-${itemIndex}`).value);
+    let currentBalance = parseFloat(document.getElementById("current-balance").innerText);
+    let donatedMoney = parseFloat(document.getElementById(`donated-money-${itemIndex}`).innerText);
+    
+
+    if (myAmount <= currentBalance && !isNaN(myAmount) && myAmount > 0) {
         donatedMoney += myAmount;
-        document.getElementById("donated-money").innerText = donatedMoney;
+        document.getElementById(`donated-money-${itemIndex}`).innerText = donatedMoney;
         currentBalance -= myAmount;
         document.getElementById("current-balance").innerText = currentBalance;
 
