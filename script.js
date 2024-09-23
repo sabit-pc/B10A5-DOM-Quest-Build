bc15c91ff146b294ee09010db5fd5d28d8e57627
sabit-pc <sabitmiah29@gmail.com>
2024-09-24 02:15:23 +0600

    blog page and more smoothness added

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ document.getElementById("donation-btn").addEventListener('click', function(){
     document.getElementById("hide-history-from-home").classList.add('hidden');
 })
 
+document.getElementById("modal-button").addEventListener('click', function(){
+    document.getElementById("donation-btn").classList.add('bg-bg-primary-0');
+})
 
 
 function donationCalculator (itemIndex) {
@@ -22,9 +25,15 @@ function donationCalculator (itemIndex) {
     let myAmount = parseFloat(document.getElementById(`my-amount-${itemIndex}`).value);
     let currentBalance = parseFloat(document.getElementById("current-balance").innerText);
     let donatedMoney = parseFloat(document.getElementById(`donated-money-${itemIndex}`).innerText);
+
+    let myTime = new Date()
+    let time = new Intl.DateTimeFormat("en-us", {
+        dateStyle: "full",
+        timeStyle: "full",
+    });
     
 
-    if (myAmount <= currentBalance && !isNaN(myAmount) && myAmount > 0) {
+    if (myAmount <= currentBalance && !isNaN(myAmount) && myAmount > 0 && typeof myAmount === 'number') {
         donatedMoney += myAmount;
         document.getElementById(`donated-money-${itemIndex}`).innerText = donatedMoney;
         currentBalance -= myAmount;
@@ -34,6 +43,7 @@ function donationCalculator (itemIndex) {
         historyContainer.innerHTML += `
         <div class="w-3/4 rounded-xl border p-8">
         <p>${myAmount} Taka is Donated for ${uniqueText}</p>
+        <p class="text-stone-600 text-sm">Date: ${time.format(myTime)}</p>
       </div>`
     }
 
